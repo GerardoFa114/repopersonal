a53a91eb419c2aa736fdc1ee8ef2e01b0762e57f
GERARDO FABIAN ALVARADO <gerardofabian23gmail.com>
2026-03-23 20:14:14 -0600

    99_run_all_local.sql origin

---
+++ infra/oracle/setup/99_run_all_local.sql
@@ -0,0 +1,8 @@
+whenever sqlerror exit failure rollback
+
+@00_drop_local_env.sql
+@01_create_local_users.sql
+@02_create_support_objects.sql
+@03_seed_local_data.sql
+@04_compile_program_units.sql
+@05_grants_for_testing.sql
